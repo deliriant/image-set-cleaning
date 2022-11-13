5dac6fc76429a1fe890f7410a7712dabb4bc3e98
Muhannad <muhannadqandah1996@gmail.com>
2022-11-14 00:42:20 +0100

    scribbles

--- preprocess.py
+++ preprocess.py
@@ -36,10 +36,8 @@ print ("After filtering out images below a height/width threshold of 800x800:", 
 print (len(img_list) - len(filtered_list), "images below threshold - discareded.")
 #print (len(squares), " images have equal height and width.")
 print ("-----------------------")
-
    
 print ("Average height and width of the filtered photos:", int(avg_height), "x", int(avg_width))
-
 
 print ("Resizing the photos that pass dimension thesholds into the average dimensions:")
 
