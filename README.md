e7e7705c13441da1b8fe24dfd33f93458194f99c
Muhannad <muhannadqandah1996@gmail.com>
2022-11-14 01:37:00 +0100

    cli tool and readme

--- README.md
+++ README.md
@@ -5,12 +5,11 @@
 - [About](#about)
 - [Getting Started](#getting_started)
 - [Usage](#usage)
-- [Contributing](../CONTRIBUTING.md)
 
 ## About <a name = "about"></a>
 
-This mini script scans recursively through a directory of images and removes the ones below a certain preset threshold of hight and width.
-Then it resizes the images that did not get discarded, into the average height and the average width of this same group of images that passed the dimension thresholds to normalize the set.
+This mini script scans recursively through a directory of images and removes the ones below a certain preset threshold of height and width, then it resizes the images that did not get discarded into the average height and the average width of this same group of images that passed the dimension thresholds to normalize the set.
+And the resulting photos are saved in a separate directory while maintaining the same folder structure as the input directory.
 
 ## Getting Started <a name = "getting_started"></a>
 
@@ -22,4 +21,8 @@ You might have to install tqdm package if you don't already have it (progress ba
 
 ## Usage <a name = "usage"></a>
 
-python3 preprocessing.py
+`python3 prep-cli.py <width-threshold> <height-threshold> <source-directory> <output-directory>`
+
+### Example
+
+`python3 prep-cli.py 800 800 Camera Finalized`
---
+++ prep-cli.py
@@ -0,0 +1,56 @@
+from PIL import Image
+import glob
+import os
+from tqdm import tqdm
+import sys
+
+img_list = list()
+filtered_list = list()
+#squares = list()
+
+width_threshold, hight_threshold = int(sys.argv[1]), int(sys.argv[2])
+heights, widths = 0, 0
+
+# Gathering file paths:
+for filepath in glob.glob(sys.argv[3]+'/**/*.jpg', recursive=True):
+    img_list.append(filepath)
+
+# moving paths of images that pass the threshold into another list: 
+for i in img_list:
+    img = Image.open(i)
+    if (img.size[0] >= hight_threshold) and (img.size[1] >= width_threshold):
+        filtered_list.append(i)
+        heights += img.size[0]
+        widths += img.size[1]
+        #if img.size[0] == img.size[1]:
+            #squares.append(img)
+
+# Calculating average height and width of all the accepted images:
+avg_height = heights/len(filtered_list)
+avg_width = widths/len(filtered_list)
+
+
+print ("Total number of images:", len(img_list), "images.")
+print ("-----------------------")
+print ("After filtering out images below a height/width threshold of 800x800:", len(filtered_list), "images remain.")
+print (len(img_list) - len(filtered_list), "images below threshold - discareded.")
+#print (len(squares), " images have equal height and width.")
+print ("-----------------------")
+   
+print ("Average height and width of the filtered images:", int(avg_height), "x", int(avg_width))
+
+print ("Resizing the images that passed the dimension thresholds to the average dimensions:")
+
+
+
+for filepath in tqdm(glob.iglob(sys.argv[3]+"/**/*.jpg", recursive=True), ascii="░▒█", desc="Processing", total=len(img_list), colour="green"):
+    if filepath in filtered_list:
+        img = Image.open(filepath)
+        new_image = img.resize((int(avg_height), int(avg_width)))
+        output_filepath = filepath.replace(sys.argv[3], sys.argv[4])
+        output_dir = os.path.dirname(output_filepath)
+        os.makedirs(output_dir, exist_ok=True)
+        img.save(output_filepath, 'JPEG')
+
+print ("-----------------------")
+print ("Saved the final set to the output directory preserving the original folder structure inside.")
